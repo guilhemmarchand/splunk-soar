a50cfa6689a0a26aefe719d40c3f37821da0ae3a
soar_local_admin <root@localhost>
2023-09-18 05:50:28 +0000

    new test

--- CRWD-0012- VT reputation utility.py
+++ CRWD-0012- VT reputation utility.py
@@ -65,26 +65,7 @@ def get_file_reputation(action=None, success=None, container=None, results=None,
     ## Custom Code End
     ################################################################################
 
-    phantom.act("file reputation", parameters=parameters, name="get_file_reputation", assets=["virustotal"], callback=add_comment_1)
-
-    return
-
-
-@phantom.playbook_block()
-def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("add_comment_1() called")
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.comment(container=container, comment="Test VT")
+    phantom.act("file reputation", parameters=parameters, name="get_file_reputation", assets=["virustotal"])
 
     return
 
