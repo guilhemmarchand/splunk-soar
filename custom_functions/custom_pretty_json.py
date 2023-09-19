b96050bb7de1c514a0d5e7cbfd0f37ec5d6eef96
Guilhem Marchand <gmarchand@splunk.com>
2023-09-19 09:37:43 +0000

    1.0.20

--- custom_functions/custom_pretty_json.py
+++ custom_functions/custom_pretty_json.py
@@ -1,7 +1,7 @@
 def custom_pretty_json(inputJson=None, **kwargs):
     """
     This custom function pretty prints a JSON provided in input
-    v1.0.19
+    v1.0.20
     
     Args:
         inputJson: Input JSON object
