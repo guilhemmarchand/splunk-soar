fb782a3b5d7000ae11fa8768eb2f4714406e94df
Guilhem Marchand <gmarchand@splunk.com>
2023-09-26 10:21:43 +0000

    demo

--- Barclays - UC-PHISHING-001 - Triage.py
+++ Barclays - UC-PHISHING-001 - Triage.py
@@ -28,13 +28,82 @@ def check_for_af_requirements(action=None, success=None, container=None, results
         conditions=[
             ["artifact:*.cef.fileHashMd5", "!=", ""],
             ["artifact:*.cef.sourceAddress", "!=", ""],
-            ["", "!=", "artifact:*.cef.requestURL"]
+            ["artifact:*.cef.requestURL", "!=", ""]
         ],
         delimiter=None)
 
     # call connected blocks if condition 1 matched
     if found_match_1:
+        filter_2(action=action, success=success, container=container, results=results, handle=handle)
         return
+
+    # check for 'else' condition 2
+    add_comment_invalid_af(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+
+@phantom.playbook_block()
+def add_comment_invalid_af(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("add_comment_invalid_af() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment="Artifact is invalid and does not contain a destinationHostname")
+
+    return
+
+
+@phantom.playbook_block()
+def filter_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_2() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.requestURL", "!=", ""]
+        ],
+        name="filter_2:condition_1",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        pass
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.sourceAddress", "!=", ""]
+        ],
+        name="filter_2:condition_2",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        pass
+
+    # collect filtered artifact ids and results for 'if' condition 3
+    matched_artifacts_3, matched_results_3 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.fileHash", "!=", ""]
+        ],
+        name="filter_2:condition_3",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_3 or matched_results_3:
+        pass
 
     return
 
