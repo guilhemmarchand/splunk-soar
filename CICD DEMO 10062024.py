ad980eb69782687eaf045b53f05ed0b2e26bf468
John Smith <jsmith@splunk.com>
2024-06-24 09:53:40 +0000

    JIRA-XXX - Demo VT

--- CICD DEMO 10062024.py
+++ CICD DEMO 10062024.py
@@ -12,14 +12,28 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'add_comment_1' block
-    add_comment_1(container=container)
+    # call 'get_vt_file_reputation' block
+    get_vt_file_reputation(container=container)
 
     return
 
 @phantom.playbook_block()
-def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("add_comment_1() called")
+def get_vt_file_reputation(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("get_vt_file_reputation() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.fileHash","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'get_vt_file_reputation' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "hash": container_artifact_item[0],
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
 
     ################################################################################
     ## Custom Code Start
@@ -31,7 +45,7 @@ def add_comment_1(action=None, success=None, container=None, results=None, handl
     ## Custom Code End
     ################################################################################
 
-    phantom.comment(container=container, comment="demo 10062024")
+    phantom.act("file reputation", parameters=parameters, name="get_vt_file_reputation", assets=["virustotal"])
 
     return
 
