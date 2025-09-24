e4a1ffc330b5b3d74fa381be65b0034810133dc5
Guilhem Marchand <gmarchand@trackme-solutions.com>
2025-09-24 13:28:28 +0000

    test

---
+++ custom_functions/get_current_epoch.py
@@ -0,0 +1,18 @@
+def get_current_epoch(**kwargs):
+    """
+    Returns a JSON-serializable object that implements the configured data paths:
+        current_epoch
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    import time
+    current_epoch = time.time()
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
